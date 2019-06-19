14afc1aaae28f071385cbcf3ed28b847b21a6cb4
annazus <suzanna@appliedar.us>
2019-06-18 17:35:14 -0700

    removed creating of data scripts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { getFirstName } from "./utils/user";
-import createData from "./migrations/migration";
+// import createData from "./migrations/migration";
 
 import server from "./server";
 server.start({ port: 4000 }, () => {
   console.log("Graphql server is up");
 });
 
-createData();
+// createData();
