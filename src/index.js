4f4017f7804bc669f3d3727e195ab306212e04fb
annazus <suzanna@appliedar.us>
2019-06-18 18:32:18 -0700

    update port server is listening on to read from config vars

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { getFirstName } from "./utils/user";
 // import createData from "./migrations/migration";
 
 import server from "./server";
-server.start({ port: 4000 }, () => {
+server.start({ port: process.env.PORT }, () => {
   console.log("Graphql server is up");
 });
 
