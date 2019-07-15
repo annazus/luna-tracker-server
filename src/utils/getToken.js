a5bf69a7dd9fb22e49d23c163ed8aedb19f752d1
annazus <suzanna@appliedar.us>
2019-07-15 13:18:41 -0700

    Return expiration time in seconds

--- src/utils/getToken.js
+++ src/utils/getToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const getToken = userId => {
-  const expiresInSeconds = 24 * 60 * 60;
+  const expiresInSeconds = 24 * 7 * 60 * 60;
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: expiresInSeconds
   });
